9a7da52943a30a7c2bdd79944cfe33be305aa871
ImAdityaa12 <adityagupta1291@gmail.com>
2024-05-03 20:30:50 +0530

    Added darkmode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="h-full flex items-center justify-center text-4xl font-bold">
       Home Page
       <UserButton afterSignOutUrl="/sign-in" />
+      <ModeToggle />
     </div>
   );
 }
